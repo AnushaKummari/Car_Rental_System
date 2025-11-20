e31fbb9aec797f20cf97c7a026e5100d18ff5695
AnushaKummari <anushakummari86@gmail.com>
2025-11-20 20:22:14 +0530

    Initial project structure with frontend and backend

---
+++ backend/Car_Rental_System_Spring/src/main/java/com/CarRental/CarRentalSystemSpringApplication.java
@@ -0,0 +1,13 @@
+package com.CarRental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRentalSystemSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarRentalSystemSpringApplication.class, args);
+	}
+
+}
